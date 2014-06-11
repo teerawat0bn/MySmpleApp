a7fcb0a4efcbde4c1be9d8dba1ce585463dd66bc
Administrator <Administrator@10.240.9.28>
2014-06-11 09:43:51 +0700

    MySmpleApp
    09:43 11/6/2557

---
+++ src/mysmpleapp/MySmpleApp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mysmpleapp;
+
+import java.util.jar.Manifest;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MySmpleApp {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        
+        
+    }
+    
+}
